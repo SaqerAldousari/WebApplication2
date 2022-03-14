a864bd3028f4ca5560e3d92d48eb8af047379a8a
Saqer Aldousari <9232@kfh.com>
2022-03-14 14:10:19 +0300

    Added the ability to login and out

--- WebApplication2/Controllers/AccountController.cs
+++ WebApplication2/Controllers/AccountController.cs
@@ -23,5 +23,35 @@ namespace WebApplication2.Controllers
         }
         public IActionResult Login() => View(new LoginVM());
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "HShowSurvey");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please, try again!";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(loginVM);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
     }
 }
--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApplication2.Data;
 using WebApplication2.Data.Services;
+using WebApplication2.Models;
 
 namespace WebApplication2
 {
